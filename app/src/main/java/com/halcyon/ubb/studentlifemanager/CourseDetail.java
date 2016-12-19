c4ea12989d4519712b45729149d27c40c67720a2
Lazar Szilard <lazar.szilard1995@gmail.com>
2016-12-19 21:51:46 +0200

    Course-Collapsing

--- app/src/main/java/com/halcyon/ubb/studentlifemanager/CourseDetail.java
+++ app/src/main/java/com/halcyon/ubb/studentlifemanager/CourseDetail.java
@@ -1,16 +1,17 @@
 package com.halcyon.ubb.studentlifemanager;
 
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,18 +21,11 @@ public class CourseDetail extends AppCompatActivity {
     private ImageView imgView;
     private TextView courseTitle, courseDescription, courseAttachment;
     private Toolbar courseToolbar;
-    private FrameLayout mFrameCourse;
-    private View mCourses;
-    private View mCoursesZoom;
+    private CollapsingToolbarLayout collapsingToolbarLayout = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_detail);
-
-        mFrameCourse = (FrameLayout) findViewById(R.id.course_opened);
-        mCourses=getLayoutInflater().inflate(R.layout.course_article_all_description, mFrameCourse, false);
-        mFrameCourse.addView(mCourses);
-
         imgView = (ImageView) findViewById(R.id.contact_image);
         courseTitle = (TextView) findViewById(R.id.courseTitle);
         courseDescription = (TextView) findViewById(R.id.courseDescription);
@@ -40,34 +34,34 @@ public class CourseDetail extends AppCompatActivity {
         courseTitle.setText(getIntent().getStringExtra("courseTitle"));
         courseDescription.setText(getIntent().getStringExtra("courseDescription"));
         courseAttachment.setText(getIntent().getStringExtra("attachmentName"));
-        //toolbar options
-        courseToolbar = (Toolbar) findViewById(R.id.toolbarr);
-        setSupportActionBar(courseToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        //supportBar options
+
         Window window = this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.supportBarColor));
-        // home button
-        final Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.ic_arrow_back);
-        upArrow.setColorFilter(ContextCompat.getColor(this, R.color.white), PorterDuff.Mode.SRC_ATOP);
-        getSupportActionBar().setHomeAsUpIndicator(upArrow);
 
-        findViewById(R.id.contact_image).setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle(" ");
+        //dynamicToolbarColor();
+
+    }
+
+    private void dynamicToolbarColor() {
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.id.contact_image);
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
             @Override
-            public void onClick(View view) {
-                mCoursesZoom=getLayoutInflater().inflate(R.layout.course_image_zoom, mFrameCourse, false);
-                mFrameCourse.addView(mCoursesZoom);
-                Picasso.with(getApplication()).load(getIntent().getStringExtra("img_id")).into((ImageView) findViewById(R.id.image_zoom));
+            public void onGenerated(Palette palette) {
+                collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(ContextCompat.getColor(getApplication(), R.color.defaultToolbarColor)));
+                collapsingToolbarLayout.setStatusBarScrimColor(palette.getMutedColor(ContextCompat.getColor(getApplication(), R.color.black)));
             }
         });
     }
-    public void zoomToThePicture(View view)
-    {
-        mFrameCourse.removeView(mCoursesZoom);
-    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId();
